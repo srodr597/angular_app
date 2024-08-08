f94654e32d7199df017d2c576f662b0f0872217a
Susell <122487002+srodr597@users.noreply.github.com>
2024-08-08 18:30:08 -0400

    Create task and user components

---
+++ review/src/app/app.component.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { UserComponent } from "./user/user.component";
+import { DUMMY_USERS } from './dummy-users';
+import { TasksComponent } from "./tasks/tasks.component";
+
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, HeaderComponent, UserComponent, TasksComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'review';
+  users = DUMMY_USERS;
+  selectedUserId = 'u1';
+  
+  
+  onSelect(id: string) {
+    this.selectedUserId = id;
+  }
+
+  get selectedUser() {
+    return this.users.find(user => user.id === this.selectedUserId)!;
+    
+  }
+
+  onSelectedId(selectedUserId: string) {
+    DUMMY_USERS.find(user => user.id === selectedUserId)! ;
+    console.log(selectedUserId)
+
+  }
+
+
+}
